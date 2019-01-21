7f1680140193cc0d5c27c34b2f31e7d78c02e641
Rizwan Tejpar <natural-affinity@users.noreply.github.com>
2019-01-21 13:50:22 -0800

    add usage and version strings with basic docopt parsing

--- main.go
+++ main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+
+	docopt "github.com/docopt/docopt-go"
 )
 
+// Version Identifier
+const Version = "0.0.1"
+
+// Usage message (docopt interface)
+const Usage = `
+  Decima (la dîme)
+    Tithe calculator
+
+  Usage:
+    decima [--percent p] [--extra e] [--breakdown] <amount>...
+    decima --help
+    decima --version
+
+  Options:
+    -h, --help          display help information
+    -v, --version       display version information
+    -b, --breakdown     display detailed breakdown
+    -e, --extra e    	add extra amount post-tithe
+    -p, --percent p     tithe percentage [default: 10]
+`
+
 func main() {
-	fmt.Println("Welcome to Decima!!!")
+	log.SetFlags(log.Lshortfile)
+
+	// parse usage string and collect args
+	_, err := docopt.ParseArgs(Usage, os.Args[1:], Version)
+	if err != nil {
+		log.Fatalf("invalid usage string: %s", err.Error())
+	}
+
+	//extract options and args
 }
